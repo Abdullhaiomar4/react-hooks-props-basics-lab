d68fa43ea20c3243bc6d2f68f30cb00b52e44319
Abdullahi Omar <abdullahiomar58@icloud.com>
2025-07-23 00:43:47 +0300

    Complete react-hooks-props-basics-lab

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,7 +2,7 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 
 import user from "../data/user";
-import App from "../components/App";
+import App from "../App";
 
 test("renders without errors", () => {
   expect(() => render(<App />)).not.toThrow();
